08488e589aef58a9f488bda4235e7d1733c219a5
Rafał Niemiec <rafal.niemiec1998@gmail.com>
2020-03-26 22:54:39 +0100

    Update lab1Sorting.py

--- lab1Sorting.py
+++ lab1Sorting.py
@@ -2,11 +2,8 @@
 #MergeSort
 
 def mergeSort(list):
-
     if len(list) > 1:
-
         i, j, k = 0, 0, 0
-
         half = len(list)//2
 
         #dividing list on two equal parts
@@ -41,7 +38,28 @@ def mergeSort(list):
     return list
 
 
-print(mergeSort([1,6,4,5,8,2,3,0,5,6,1, 56, 70, 34, 26, 11, -4, -5, 11]))
+#smallest element method
+
+def simpleSort(list):
+    for i in range(len(list)):
+        smallInd = i       #position of smallest element
+
+        for j in range(i + 1, len(list)):
+            if list[smallInd] > list[j]:
+                smallInd = j
+
+        #swap lowest value with i element
+        list[i], list[smallInd] = list[smallInd], list[i]
+
+    return(list)
 
 
+def sortTest(test):
+    print("Tested array: ", test)
+    print("Simple sort result: ", simpleSort(test))
+    print("Merge sort result: ", mergeSort(test))
+    print(100*"-")
 
+sortTest([-100, 1, 50, 60, 70, 12, 12, 45, 43, 23, 22, 56, 8, 5, 2, 3])
+sortTest([-3, -5, 6, 10, 5, 6, 10, 50, 20, 22, 10, 5, -1, 132, 193, 5, 0])
+sortTest([0, -4.2, -1.6, -1, 4.1, 5.7, 3.2])
