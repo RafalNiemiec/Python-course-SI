e96e33175e40948c144f3ad8e3d6851051938e1a
RafalNiemiec <rafal.niemiec1998@gmail.com>
2020-04-28 00:03:08 +0200

    Add min/max crypto value

--- l4.py
+++ l4.py
@@ -1,42 +1,26 @@
 import requests
 
-wallet = 0
 
-def getData(amountMoney):
-    buyList = [
-        float(requests.get('https://bitbay.net/API/Public/BTCUSD/ticker.json').json()['ask']),
-        float(requests.get('https://api.bittrex.com/api/v1.1/public/getticker?market=USD-BTC').json()['result']['Ask']),
-        float(requests.get('https://api.coinbase.com/v2/prices/BTC-USD/buy').json()['data']['amount']),
-        float(requests.get('https://www.bitstamp.net/api/ticker').json()['ask'])
-    ]
-    sellList = [
-        float(requests.get('https://bitbay.net/API/Public/BTCUSD/ticker.json').json()['bid']),
-        float(requests.get('https://api.bittrex.com/api/v1.1/public/getticker?market=USD-BTC').json()['result']['Bid']),
-        float(requests.get('https://api.coinbase.com/v2/prices/BTC-USD/sell').json()['data']['amount']),
-        float(requests.get('https://www.bitstamp.net/api/ticker').json()['bid'])
-    ]
-    #fees = [BitBay, BitTrex, CoinBase, BitStamp]
-    fees = [0.01, 0.002, 0.005, 0.005]
+btc = requests.get('https://bitbay.net/API/Public/BTCUSD/ticker.json').json()
+print(btc['max'])
+print(btc['min'])
+btcProfit = (btc['max']/btc['min'] - 1)*100
+print(round(btcProfit,2))
 
-    trading(min(buyList), max(sellList), amountMoney, fees[buyList.index(min(buyList))])
+eth = requests.get('https://bitbay.net/API/Public/ETHUSD/ticker.json').json()
+print(eth['max'])
+print(eth['min'])
+ethProfit = (eth['max']/eth['min'] - 1)*100
+print(round(ethProfit,2))
 
+ltc = requests.get('https://bitbay.net/API/Public/LTCUSD/ticker.json').json()
+print(ltc['max'])
+print(ltc['min'])
+ltcProfit = (ltc['max']/ltc['min'] - 1)*100
+print(round(ltcProfit,2))
 
-def trading(buy, sell, amountMoney, fee):
-    arbitrage = 0
-    arbitrage += amountMoney * (sell - buy - sell*fee)
-    global wallet
-    if arbitrage > 0:
-        print('Arbitrage is possible, with income on level: ', arbitrage)
-        wallet += arbitrage
-    else:
-        print('Arbitrage is not possible. \nArbitragee level: ', arbitrage)
-
-    print('Money in walet: ', wallet, '\n', 15 * '-')
-
-
-def makeMoney(amountBTC):
-    while True:
-        getData(amountBTC)
-
-
-makeMoney(0.2)
+dash = requests.get('https://bitbay.net/API/Public/DASHUSD/ticker.json').json()
+print(dash['max'])
+print(dash['min'])
+dashProfit = (dash['max']/dash['min'] - 1)*100
+print(round(dashProfit,2))
