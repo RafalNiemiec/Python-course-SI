b415b7a2ee425b9e32b2086e5c71ed69ba3c22b4
RafalNiemiec <rafal.niemiec1998@gmail.com>
2020-04-27 20:14:08 +0200

    Add calculating arbitrage

--- l4.py
+++ l4.py
@@ -1,34 +1,38 @@
 import requests
 
-#BitBay
-#print('BitBay sell: ', requests.get('https://bitbay.net/API/Public/BTCUSD/ticker.json').json()['bid'])
-#print('BitBay buy: ', requests.get('https://bitbay.net/API/Public/BTCUSD/ticker.json').json()['ask'])
-
-#BITTREX
-#print("Bittrex buy: ", requests.get('https://api.bittrex.com/api/v1.1/public/getticker?market=USD-BTC').json()['result']['Bid'])
-#print("Bittrex sell: ", requests.get('https://api.bittrex.com/api/v1.1/public/getticker?market=USD-BTC').json()['result']['Ask'])
-
-#dane wyciągane z COINBASE
-#print('Coinbase buy: ', requests.get('https://api.coinbase.com/v2/prices/BTC-USD/buy').json()['data']['amount'])
-#print('Coinbase sell: ', requests.get('https://api.coinbase.com/v2/prices/BTC-USD/sell').json()['data']['amount'])
 
 
-buyList = [
-    float(requests.get('https://bitbay.net/API/Public/BTCUSD/ticker.json').json()['ask']),
-    float(requests.get('https://api.bittrex.com/api/v1.1/public/getticker?market=USD-BTC').json()['result']['Ask']),
-    float(requests.get('https://api.coinbase.com/v2/prices/BTC-USD/buy').json()['data']['amount'])
-]
+def getData():
+    buyList = [
+        float(requests.get('https://bitbay.net/API/Public/BTCUSD/ticker.json').json()['ask']),
+        float(requests.get('https://api.bittrex.com/api/v1.1/public/getticker?market=USD-BTC').json()['result']['Ask']),
+        float(requests.get('https://api.coinbase.com/v2/prices/BTC-USD/buy').json()['data']['amount']),
+        float(requests.get("https://api.coinbase.com/v2/prices/BTC-USD/buy").json()['data']['amount'])
+    ]
+    sellList = [
+        float(requests.get('https://bitbay.net/API/Public/BTCUSD/ticker.json').json()['bid']),
+        float(requests.get('https://api.bittrex.com/api/v1.1/public/getticker?market=USD-BTC').json()['result']['Bid']),
+        float(requests.get('https://api.coinbase.com/v2/prices/BTC-USD/sell').json()['data']['amount']),
+        float(requests.get("https://api.coinbase.com/v2/prices/BTC-USD/sell").json()['data']['amount'])
+    ]
 
-sellList = [
-    float(requests.get('https://bitbay.net/API/Public/BTCUSD/ticker.json').json()['bid']),
-    float(requests.get('https://api.bittrex.com/api/v1.1/public/getticker?market=USD-BTC').json()['result']['Bid']),
-    float(requests.get('https://api.coinbase.com/v2/prices/BTC-USD/sell').json()['data']['amount'])
-]
+    trading(min(buyList), max(sellList), 0.01)
 
-buy = min(buyList)
-sell = max(sellList)
-print(buy)
-print(sell)
 
-print(0.1*buy-0.1*sell)
+def trading(buy, sell, amountMoney):
+    arbitrage = 0
+    arbitrage += amountMoney * (sell - buy - sell*0.001)
+    global walet
+    if arbitrage > 0:
+        print('Arbitrage is possible, with income on level: ', arbitrage)
+        walet += arbitrage
+    else:
+        print('Arbitrage is not possible. \nArbitragee level: ', arbitrage)
 
+    print('Money in walet: ', walet, '\n', 15*'-')
+
+
+
+walet = 0
+while True:
+    getData()
