26a2720bf435693135a9dd70cebeee6aaafd98ec
RafalNiemiec <rafal.niemiec1998@gmail.com>
2020-05-01 17:19:19 +0200

    Add downloading data

--- l5.py
+++ l5.py
@@ -1,26 +1,36 @@
 import requests
 
+def getData():
+    btc = requests.get('https://bitbay.net/API/Public/BTCUSD/ticker.json').json()
+    eth = requests.get('https://bitbay.net/API/Public/ETHUSD/ticker.json').json()
+    ltc = requests.get('https://bitbay.net/API/Public/LTCUSD/ticker.json').json()
+    dash = requests.get('https://bitbay.net/API/Public/DASHUSD/ticker.json').json()
 
-btc = requests.get('https://bitbay.net/API/Public/BTCUSD/ticker.json').json()
+
 print(btc['max'])
 print(btc['min'])
 btcProfit = (btc['max']/btc['min'] - 1)*100
 print(round(btcProfit,2))
 
-eth = requests.get('https://bitbay.net/API/Public/ETHUSD/ticker.json').json()
+
 print(eth['max'])
 print(eth['min'])
 ethProfit = (eth['max']/eth['min'] - 1)*100
 print(round(ethProfit,2))
 
-ltc = requests.get('https://bitbay.net/API/Public/LTCUSD/ticker.json').json()
+
 print(ltc['max'])
 print(ltc['min'])
 ltcProfit = (ltc['max']/ltc['min'] - 1)*100
 print(round(ltcProfit,2))
 
-dash = requests.get('https://bitbay.net/API/Public/DASHUSD/ticker.json').json()
+
 print(dash['max'])
 print(dash['min'])
 dashProfit = (dash['max']/dash['min'] - 1)*100
 print(round(dashProfit,2))
+
+def write():
+
+
+
