ae4519299837af4c7eb9a06e508cb71d4b0f239f
RafalNiemiec <rafal.niemiec1998@gmail.com>
2020-05-02 18:22:40 +0200

    Add income sort

--- l5.py
+++ l5.py
@@ -3,34 +3,36 @@ import requests
 def getData():
     btc = requests.get('https://bitbay.net/API/Public/BTCUSD/ticker.json').json()
     eth = requests.get('https://bitbay.net/API/Public/ETHUSD/ticker.json').json()
-    ltc = requests.get('https://bitbay.net/API/Public/LTCUSD/ticker.json').json()
-    dash = requests.get('https://bitbay.net/API/Public/DASHUSD/ticker.json').json()
+    neu = requests.get('https://bitbay.net/API/Public/NEUUSD/ticker.json').json()
+    lsk = requests.get('https://bitbay.net/API/Public/LSKUSD/ticker.json').json()
+
+    profitList = []
+
+    #btcProfit
+    profitList.append([round((btc['max']/btc['min'] - 1)*100, 2), 'BTC'])
+
+    #ethProfit
+    profitList.append([round((eth['max']/eth['min'] - 1)*100, 2), 'ETH'])
+
+    #neuProfit
+    profitList.append([round((neu['max']/neu['min'] - 1)*100, 2), 'LTC'])
+
+    #lskProfit
+    profitList.append([round((lsk['max']/lsk['min'] - 1)*100, 2), 'LSK'])
+
+    sort(profitList)
 
 
-print(btc['max'])
-print(btc['min'])
-btcProfit = (btc['max']/btc['min'] - 1)*100
-print(round(btcProfit,2))
+def sort(profitList):
+    for j in range(len(profitList)-1):
+        for i in range(len(profitList)-1):
+            if profitList[i][0] < profitList[i+1][0]:
+                profitList[i], profitList[i+1] = profitList[i+1], profitList[i]
+    write(profitList)
 
 
-print(eth['max'])
-print(eth['min'])
-ethProfit = (eth['max']/eth['min'] - 1)*100
-print(round(ethProfit,2))
+def write(profitList):
+    for i in profitList:
+        print(i[1], ' ', i[0], '%')
 
-
-print(ltc['max'])
-print(ltc['min'])
-ltcProfit = (ltc['max']/ltc['min'] - 1)*100
-print(round(ltcProfit,2))
-
-
-print(dash['max'])
-print(dash['min'])
-dashProfit = (dash['max']/dash['min'] - 1)*100
-print(round(dashProfit,2))
-
-def write():
-
-
-
+getData()
