f8b87e5d3dddfaeee5a3917ec236d1457154b62a
RafalNiemiec <rafal.niemiec1998@gmail.com>
2020-05-23 17:42:57 +0200

    Finish prediction model

--- volumeSimulating.py
+++ volumeSimulating.py
@@ -1,28 +1,61 @@
 import time
 import requests
+from scipy.stats import norm
+import matplotlib.pyplot as plt
+from random import *
+from statistics import median
+from numpy import *
 
-def getData(timer, crypto):
-    volumens = []
-    difference = []
-    url = "https://www.bitstamp.net/api/v2/ohlc/btcusd?step=86400&limit=10&start={timer}".format(timer=timer)
-    print(url)
-    dailyData = requests.get(url).json()['data']['ohlc']
+predictList, vol, difference = [], [], []
 
-
-    for day in dailyData:
-        volumens.append([float(day['volume']), time.ctime(float(day['timestamp']))])
-
-    for i in range(0, len(volumens)):
-        difference.append((volumens[i][0]-volumens[i-1][0])/volumens[i][0])
-
-    print(difference)
+def getData(pastDays, crypto):
+    url = "https://www.bitstamp.net/api/v2/ohlc/{crypto}usd?step=86400&limit=100&start={timer}".format(
+        timer=str(int(time.time()) - 86400 * pastDays), crypto=crypto)
+    return requests.get(url).json()['data']['ohlc']
 
 
+def information(dailyData):
+    for i in range(0, len(dailyData) - 1):
+        vol.append(float(dailyData[i]['volume']))
+        difference.append(absolute((vol[i] - vol[i - 1]) / vol[i]))
+    return difference, vol
 
 
+def absolute(num):
+    if num > 1:
+        return num - 1
+    if num < -1:
+        return num + 1
+    return num
 
 
-timer = int(time.time())-86400*20
+def simulation(data, volumens):
+    avg, std = norm.fit(data)
+    predict = absolute(gauss(avg, std))
+    predictList.append(predict * volumens[-1] + volumens[-1])
+    plt.plot(arange(0, len(volumens)), volumens, color='g')
+    plt.plot(arange(len(volumens), len(predictList) + len(volumens)), predictList, color='b')
+    plt.grid(True)
+    plt.xlabel("Days")
+    plt.ylabel("Volume")
+    plt.title("Prediction of volume")
+    plt.legend(['Historical data','Predicted data'])
 
-getData(str(timer), 'BTC')
 
+def pointer(predictList):
+    med = median(predictList)
+    avg, std = norm.fit(predictList)
+    print('One simulation: ', predictList[0])
+    print('Madian: ', med, '\nAvarange: ', avg, '\nStd: ', std)
+
+
+def calculate(daysAnalyst, currency):
+    difference, vol = information(getData(daysAnalyst, currency))
+    for i in range(daysAnalyst):
+        simulation(difference, vol)
+    pointer(predictList)
+    plt.show()
+
+
+#calculate(how many days You want to calculate, currency)
+calculate(100, 'btc')
